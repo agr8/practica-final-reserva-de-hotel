d37057862e1f6205d472de6886dde5cb1f2a078f
Ángela Gallardo Ruiz <gallardoangela.ruiz@gmail.com>
2023-03-22 18:09:43 +0100

    Añadidos Availability Controller and Service

---
+++ reserva-hoteles/src/main/java/springboot/api/rest/reserva/hoteles/controller/AvailabilityController.java
@@ -0,0 +1,10 @@
+package springboot.api.rest.reserva.hoteles.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("disponibilidad")
+public class AvailabilityController {
+	
+}
---
+++ reserva-hoteles/src/main/java/springboot/api/rest/reserva/hoteles/service/impl/AvailabilityServiceImpl.java
@@ -0,0 +1,7 @@
+package springboot.api.rest.reserva.hoteles.service.impl;
+
+import springboot.api.rest.reserva.hoteles.service.AvailabilityService;
+
+public class AvailabilityServiceImpl implements AvailabilityService {
+
+}
