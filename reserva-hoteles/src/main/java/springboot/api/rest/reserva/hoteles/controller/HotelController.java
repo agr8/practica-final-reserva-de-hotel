17f8f29b7b48f2f4b4d41b84179e3ecbe14e9f5c
Ángela Gallardo Ruiz <gallardoangela.ruiz@gmail.com>
2023-03-22 20:08:34 +0100

    Añade Hotel Controller, Service and DTO

---
+++ reserva-hoteles/src/main/java/springboot/api/rest/reserva/hoteles/controller/HotelController.java
@@ -0,0 +1,11 @@
+package springboot.api.rest.reserva.hoteles.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("hoteles")
+public class HotelController {
+	
+}
---
+++ reserva-hoteles/src/main/java/springboot/api/rest/reserva/hoteles/service/impl/HotelServiceImpl.java
@@ -0,0 +1,7 @@
+package springboot.api.rest.reserva.hoteles.service.impl;
+
+import springboot.api.rest.reserva.hoteles.service.HotelService;
+
+public class HotelServiceImpl implements HotelService {
+
+}
