934bf015db681be567af47f2e2c4c0b06256742f
Ángela Gallardo Ruiz <gallardoangela.ruiz@gmail.com>
2023-03-22 10:39:14 +0100

    Añade proyecto base

---
+++ reserva-hoteles/src/main/java/springboot/api/rest/reserva/hoteles/ReservaHotelesApplication.java
@@ -0,0 +1,13 @@
+package springboot.api.rest.reserva.hoteles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReservaHotelesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReservaHotelesApplication.class, args);
+	}
+
+}
---
+++ reserva-hoteles/src/test/java/springboot/api/rest/reserva/hoteles/ReservaHotelesApplicationTests.java
@@ -0,0 +1,13 @@
+package springboot.api.rest.reserva.hoteles;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReservaHotelesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
