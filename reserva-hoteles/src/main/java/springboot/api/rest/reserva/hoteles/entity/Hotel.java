6abf96e85d73b7500f15d1c9d9bcc2df77bd3741
Ángela Gallardo Ruiz <gallardoangela.ruiz@gmail.com>
2023-03-22 16:22:46 +0100

    Añadido Hotel Entity

---
+++ reserva-hoteles/src/main/java/springboot/api/rest/reserva/hoteles/entity/Hotel.java
@@ -0,0 +1,45 @@
+package springboot.api.rest.reserva.hoteles.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "hotels")
+public class Hotel {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
+	private Integer id;
+	@Column(name = "name")
+	private String name;
+	@Column(name = "category")
+	private String category;
+	
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getCategory() {
+		return category;
+	}
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	
+	
+	
+}
