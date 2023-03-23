91be53c85728f3bceb26435cb32b88a514d572bb
Ángela Gallardo Ruiz <gallardoangela.ruiz@gmail.com>
2023-03-23 19:09:37 +0100

    Añade configuracion

--- reserva-hoteles/src/main/java/springboot/api/rest/reserva/hoteles/ReservaHotelesApplication.java
+++ reserva-hoteles/src/main/java/springboot/api/rest/reserva/hoteles/ReservaHotelesApplication.java
@@ -2,12 +2,24 @@ package springboot.api.rest.reserva.hoteles;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 
+import com.fasterxml.jackson.datatype.hibernate5.Hibernate5Module;
+
+@ComponentScan("springboot.api.rest.reserva.hoteles")
 @SpringBootApplication
 public class ReservaHotelesApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ReservaHotelesApplication.class, args);
 	}
-
+	
+	@Bean
+	public Hibernate5Module hibernateModule() {
+		Hibernate5Module module = new Hibernate5Module();
+		module.disable(Hibernate5Module.Feature.USE_TRANSIENT_ANNOTATION);
+		module.enable(Hibernate5Module.Feature.FORCE_LAZY_LOADING);
+		return module;
+	}
 }
