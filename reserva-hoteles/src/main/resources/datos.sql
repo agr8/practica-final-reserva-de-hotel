2ec05e05be6f0270b943730fa0a001c8edc36a13
Ángela Gallardo Ruiz <gallardoangela.ruiz@gmail.com>
2023-03-26 20:43:06 +0200

    Añade ficheros postgresql

---
+++ reserva-hoteles/src/main/resources/datos.sql
@@ -0,0 +1,33 @@
+INSERT INTO public.hotels(name, category) VALUES
+	('Hotel Playa Resort', 1),
+	('Hotel Playa Club', 1),
+	('Hotel Centro Granada', 2),
+	('Hotel Rural Montaña', 3),
+	('Hotel Rural Pueblo', 3);
+
+INSERT INTO public.availabilities("date", id_hotel, rooms) VALUES
+	('13-10-22', 1, 5),
+	('14-10-22', 1, 5),
+	('15-10-22', 1, 5),
+	('13-10-22', 2, 10),
+	('14-10-22', 2, 8),
+	('15-10-22', 2, 8),
+	('13-10-22', 3, 0),
+	('14-10-22', 3, 0),
+	('15-10-22', 3, 0),
+	('13-10-22', 4, 3),
+	('14-10-22', 4, 3),
+	('15-10-22', 4, 0),
+	('16-10-22', 4, 1),
+	('13-10-22', 5, 1),
+	('14-10-22', 5, 1),
+	('15-10-22', 5, 0),
+	('16-10-22', 5, 0);
+	
+/*
+INSERT INTO public.bookings(id_hotel, date_from, date_to, email) VALUES
+	(1, ?, ?, 'pepe_48@gmail.com'),
+	(1, ?, ?, 'rosajp@gmail.com'),
+	(2, ?, ?, 'maria6739@gmail.com'),
+	(4, ?, ?, 'cristinaui_98@gmail.com');
+*/
